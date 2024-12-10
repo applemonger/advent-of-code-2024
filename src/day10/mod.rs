237e792886074d0e045c2d4857ea696719d8e679
Joseph Nawrocki <jonawrocki@gmail.com>
2024-12-10 12:04:50 -0500

    cleanup

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -4,50 +4,45 @@ use aocd::*;
 
 #[derive(Clone)]
 struct Grid {
-    values: Vec<Option<usize>>,
-    width: i32,
-    height: i32,
+    values: HashMap<(i32, i32), Option<usize>>,
 }
 
 impl Grid {
     fn new(input: String) -> Grid {
-        let values: Vec<Option<usize>> = input
+        let values: HashMap<(i32, i32), Option<usize>> = input
             .lines()
-            .flat_map(|s| s.chars())
-            .map(|c| c.to_digit(10).map(|x| x as usize))
+            .enumerate()
+            .flat_map(|(y, line)| {
+                line.chars().enumerate().map(move |(x, c)| {
+                    let value = c.to_digit(10);
+                    ((x as i32, y as i32), value.map(|num| num as usize))
+                })
+            })
             .collect();
-        let width = input.lines().next().unwrap().len() as i32;
-        let height = values.len() as i32 / width;
-        Grid {
-            values,
-            width,
-            height,
-        }
+        Grid { values }
     }
 
     fn get(&self, xy: (i32, i32)) -> Option<usize> {
-        if (0..self.width).contains(&xy.0) && (0..self.height).contains(&xy.1) {
-            let index = xy.1 * self.width + xy.0;
-            *self.values.get(index as usize).unwrap()
-        } else {
-            None
-        }
+        *self.values.get(&xy)?
     }
 
-    fn score(&self, mut nines: HashMap<(i32, i32), i32>, start: (i32, i32)) -> HashMap<(i32, i32), i32> {
+    fn score(
+        &self,
+        mut nines: HashMap<(i32, i32), i32>,
+        start: (i32, i32),
+    ) -> HashMap<(i32, i32), i32> {
         let current = self.get(start).unwrap();
         for (u, v) in [(-1, 0), (1, 0), (0, 1), (0, -1)] {
             let (x, y) = (start.0 + u, start.1 + v);
             if let Some(num) = self.get((x, y)) {
                 if num == current + 1 {
                     if num == 9 {
-                        let rating = nines.entry((x, y)).or_default();
-                        *rating += 1;
+                        *nines.entry((x, y)).or_default() += 1;
                     } else {
                         nines = self.score(nines, (x, y));
                     }
                 }
-            }       
+            }
         }
         nines
     }
@@ -58,12 +53,10 @@ pub fn solution1() {
     let data = input!();
     let grid = Grid::new(data);
     let mut score = 0;
-    for x in 0..grid.width {
-        for y in 0..grid.height {
-            if let Some(0) = grid.get((x, y)) {
-                let nines = grid.score(HashMap::new(), (x, y));
-                score += nines.len();
-            }
+    for (&position, value) in grid.values.iter() {
+        if let Some(0) = value {
+            let nines = grid.score(HashMap::new(), position);
+            score += nines.len();
         }
     }
     submit!(1, score);
@@ -74,12 +67,10 @@ pub fn solution2() {
     let data = input!();
     let grid = Grid::new(data);
     let mut rating = 0;
-    for x in 0..grid.width {
-        for y in 0..grid.height {
-            if let Some(0) = grid.get((x, y)) {
-                let nines = grid.score(HashMap::new(), (x, y));
-                rating += nines.iter().map(|(_, v)| *v).sum::<i32>();
-            }
+    for (&position, value) in grid.values.iter() {
+        if let Some(0) = value {
+            let nines = grid.score(HashMap::new(), position);
+            rating += nines.values().copied().sum::<i32>();
         }
     }
     submit!(2, rating);
