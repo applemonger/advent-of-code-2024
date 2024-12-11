21d66ce6acb6e67f4bdf20949da0a2f407ff4fdb
Joseph Nawrocki <jonawrocki@gmail.com>
2024-12-11 09:53:39 -0500

    fmt

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -11,14 +11,14 @@ fn blink_stone(stone: u64, blinks: u64) -> u64 {
     let mut count = 0;
     if blinks > 0 {
         if stone == 0 {
-            count += blink_stone(1, blinks-1);
+            count += blink_stone(1, blinks - 1);
         } else if count_digits(stone) % 2 == 0 {
             let n = count_digits(stone);
             let divisor = 10_u64.pow(n / 2);
-            count += blink_stone(stone / divisor, blinks-1);
-            count += blink_stone(stone % divisor, blinks-1);
+            count += blink_stone(stone / divisor, blinks - 1);
+            count += blink_stone(stone % divisor, blinks - 1);
         } else {
-            count += blink_stone(stone * 2024, blinks-1);
+            count += blink_stone(stone * 2024, blinks - 1);
         }
     } else {
         count += 1;
@@ -28,12 +28,30 @@ fn blink_stone(stone: u64, blinks: u64) -> u64 {
 
 #[aocd(2024, 11)]
 pub fn solution1() {
-    let stones: Vec<u64> = input!().split(' ').map(|num| num.parse().unwrap()).collect();
-    submit!(1, stones.iter().map(|stone| blink_stone(*stone, 25)).sum::<u64>());
+    let stones: Vec<u64> = input!()
+        .split(' ')
+        .map(|num| num.parse().unwrap())
+        .collect();
+    submit!(
+        1,
+        stones
+            .iter()
+            .map(|stone| blink_stone(*stone, 25))
+            .sum::<u64>()
+    );
 }
 
 #[aocd(2024, 11)]
 pub fn solution2() {
-    let stones: Vec<u64> = input!().split(' ').map(|num| num.parse().unwrap()).collect();
-    submit!(2, stones.iter().map(|stone| blink_stone(*stone, 75)).sum::<u64>());
+    let stones: Vec<u64> = input!()
+        .split(' ')
+        .map(|num| num.parse().unwrap())
+        .collect();
+    submit!(
+        2,
+        stones
+            .iter()
+            .map(|stone| blink_stone(*stone, 75))
+            .sum::<u64>()
+    );
 }
