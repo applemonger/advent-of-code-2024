22807b898a185f030134562c85151b05316e9e45
Joseph Nawrocki <jonawrocki@gmail.com>
2024-12-12 16:41:13 -0500

    day 12 pt 2 horrendous

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -14,11 +14,63 @@ impl Garden {
 
     fn neighbors(&self) -> HashSet<Garden> {
         let adjacent = [(-1, 0), (1, 0), (0, 1), (0, -1)];
-        adjacent.iter().map(|&(x, y)| self.offset(x, y)).collect()
+        adjacent.iter().map(|&pos| self.offset(pos)).collect()
     }
 
-    fn offset(&self, x: i32, y: i32) -> Garden {
-        Garden::new(self.x + x, self.y + y)
+    fn offset(&self, offset: (i32, i32)) -> Garden {
+        Garden::new(self.x + offset.0, self.y + offset.1)
+    }
+
+    fn sides(&self, region: &Region) -> HashSet<Side> {
+        let mut sides = HashSet::<Side>::new();
+        for direction in [(-1, 0), (1, 0), (0, 1), (0, -1)] {
+            let neighbor = self.offset(direction);
+            if !region.gardens.contains(&neighbor) {
+                sides.insert(Side { position: *self, direction });
+            }
+        }
+        sides
+    }
+}
+
+#[derive(Debug, Hash, Clone, Copy, PartialEq, Eq)]
+struct Side {
+    position: Garden,
+    direction: (i32, i32)
+}
+
+impl Side {
+    fn vertical(&self) -> bool {
+        self.direction.1 != 0
+    }
+
+    fn is_connected(&self, other: &Side, sides: &HashSet<Side>) -> bool {
+        if self.position == other.position {
+            return false;
+        } else if self.position.x == other.position.x && self.direction.0 != 0 {
+            let y_min = self.position.y.min(other.position.y) + 1;
+            let y_max = self.position.y.max(other.position.y);
+            for y in y_min..y_max {
+                let garden = Garden::new(self.position.x, y);
+                let side = Side { position: garden, direction: self.direction };
+                if !sides.contains(&side) {
+                    return false;
+                }
+            }
+        } else if self.position.y == other.position.y && self.direction.1 != 0 {
+            let x_min = self.position.x.min(other.position.x) + 1;
+            let x_max = self.position.x.max(other.position.x);
+            for x in x_min..x_max {
+                let garden = Garden::new(x, self.position.y);
+                let side = Side { position: garden, direction: self.direction };
+                if !sides.contains(&side) {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+        self.direction == other.direction
     }
 }
 
@@ -43,6 +95,25 @@ impl Region {
             .map(|garden| garden.neighbors().difference(&self.gardens).count())
             .sum();
         perimeter * self.gardens.len()
+    }
+
+    fn discounted_cost(&self) -> usize {
+        let mut sides: HashSet<Side> = self.gardens.iter().flat_map(|garden| garden.sides(&self)).collect();
+        //println!("{} {}", self.plant, sides.len());
+        let all_sides = sides.clone();
+        let mut count = 0;
+        while let Some(side) = sides.iter().next().copied() {
+            //println!("{} {:?}", self.plant, side);
+            for other in all_sides.clone().iter().copied() {
+                if side.is_connected(&other, &all_sides) {
+                    //println!("-> {:?}", other);
+                    sides.remove(&other);
+                }
+            }
+            sides.remove(&side);
+            count += 1;
+        }
+        count * self.gardens.len()
     }
 
     fn build(&mut self, current: Garden, grid: &mut Grid) {
@@ -100,5 +171,10 @@ pub fn solution1() {
     submit!(1, score);
 }
 
-#[aocd(2024, 12, "src/day12/test2.txt")]
-pub fn solution2() {}
+#[aocd(2024, 12)]
+pub fn solution2() {
+    let data = input!();
+    let regions = Grid::new(data).to_regions();
+    let score = regions.iter().map(|region| region.discounted_cost()).sum::<usize>();
+    submit!(2, score);
+}
