c60d2e4b07a3c40bbb26ecdac35a9105bd715562
Joseph Nawrocki <jonawrocki@gmail.com>
2024-12-19 22:10:19 -0500

    cleanup

--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -1,18 +1,18 @@
 use aocd::*;
 use cached::proc_macro::cached;
 
-fn read_data(data: String) -> (Vec<String>, Vec<String>) {
+fn read_data(data: &str) -> (Vec<&str>, Vec<&str>) {
     let input: Vec<&str> = data.split("\n\n").collect();
-    let patterns: Vec<String> = input[0].split(", ").map(|s| s.to_string()).collect();
-    let designs: Vec<String> = input[1].lines().map(|s| s.to_string()).collect();
+    let patterns: Vec<&str> = input[0].split(", ").collect();
+    let designs: Vec<&str> = input[1].lines().collect();
     (patterns, designs)
 }
 
-fn get_tokens(input: &str, dictionary: &[String]) -> Vec<String> {
+fn get_tokens(input: &str, dictionary: &[&str]) -> Vec<String> {
     dictionary
         .iter()
         .filter(|&token| input.contains(token))
-        .cloned()
+        .map(|s| s.to_string())
         .collect()
 }
 
@@ -35,10 +35,10 @@ fn reduce(input: String, tokens: Vec<String>) -> usize {
 #[aocd(2024, 19)]
 pub fn solution1() {
     let data = input!();
-    let (dictionary, designs) = read_data(data);
+    let (dictionary, designs) = read_data(data.as_str());
     let mut total = 0;
     for design in designs {
-        total += (reduce(design.clone(), get_tokens(&design, &dictionary)) > 0) as usize;
+        total += (reduce(design.to_string(), get_tokens(design, &dictionary)) > 0) as usize;
     }
     submit!(1, total);
 }
@@ -46,10 +46,10 @@ pub fn solution1() {
 #[aocd(2024, 19)]
 pub fn solution2() {
     let data = input!();
-    let (dictionary, designs) = read_data(data);
+    let (dictionary, designs) = read_data(data.as_str());
     let mut total = 0;
     for design in designs {
-        total += reduce(design.clone(), get_tokens(&design, &dictionary));
+        total += reduce(design.to_string(), get_tokens(design, &dictionary));
     }
     submit!(2, total);
 }
