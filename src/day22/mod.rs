ee69308dd7cc247040ac16580d7224ca059c8aac
Joseph Nawrocki <jonawrocki@gmail.com>
2024-12-22 17:46:40 -0500

    cleanup

--- src/day22/mod.rs
+++ src/day22/mod.rs
@@ -4,30 +4,21 @@ use aocd::*;
 
 type Seq = (isize, isize, isize, isize);
 
-fn gen_last(mut num: usize, n: usize) -> usize {
-    for _ in 0..n {
-        num = gen(num);
-    }
-    num
-}
-
 fn gen(mut num: usize) -> usize {
     num ^= num * 64;
     num %= 16777216;
     num ^= num / 32;
     num %= 16777216;
     num ^= num * 2048;
-    num %= 16777216;
-    num
+    num % 16777216
 }
 
-fn seq_map(mut num: usize, n: usize) -> HashMap<Seq, usize> {
-    let mut prices = vec![num as isize % 10];
-    for _ in 0..n {
-        num = gen(num);
-        let price = num % 10;
-        prices.push(price as isize);
-    }
+fn seq_map(seed: usize, n: usize) -> HashMap<Seq, usize> {
+    let mut prices = Vec::new();
+    (0..n).fold(seed, |num, _| {
+        prices.push(num as isize % 10);
+        gen(num)
+    });
     let diff: Vec<isize> = prices.windows(2).map(|x| x[1] - x[0]).collect();
     let mut changes = HashMap::<Seq, usize>::new();
     diff.windows(4).enumerate().for_each(|(i, diffs)| {
@@ -37,18 +28,12 @@ fn seq_map(mut num: usize, n: usize) -> HashMap<Seq, usize> {
     changes
 }
 
-fn merge(mut a: HashMap<Seq, usize>, b: HashMap<Seq, usize>) -> HashMap<Seq, usize> {
-    for (k, v) in b {
-        *a.entry(k).or_insert(0) += v;
-    }
-    a
-}
-
 #[aocd(2024, 22)]
 pub fn solution1() {
     let data = input!();
-    let secrets: Vec<usize> = data.lines().map(|x| x.parse().unwrap()).collect();
-    let total: usize = secrets.iter().map(|&x| gen_last(x, 2000)).sum();
+    let seeds: Vec<usize> = data.lines().map(|x| x.parse().unwrap()).collect();
+    let k = 2000;
+    let total: usize = seeds.iter().map(|&x| (0..k).fold(x, |n, _| gen(n))).sum();
     submit!(1, total);
 }
 
@@ -58,8 +43,9 @@ pub fn solution2() {
     let secrets: Vec<usize> = data.lines().map(|x| x.parse().unwrap()).collect();
     let mut totals = HashMap::<Seq, usize>::new();
     for secret in secrets {
-        let map = seq_map(secret, 2000);
-        totals = merge(totals, map);
+        for (seq, price) in seq_map(secret, 2000) {
+            *totals.entry(seq).or_insert(0) += price;
+        }
     }
     let (_, best) = totals.iter().max_by_key(|&(_, &v)| v).unwrap();
     submit!(2, *best);
